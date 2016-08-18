f08be08ad4b347961bbab59a5f69cc0aa1aeff71
LumberZack <zack.poirier88@gmail.com>
2016-08-18 11:23:47 -0400

    Switched from Tester Page to First Real Page
    Successfully tested

--- TouringVancouver/app/src/main/java/com/example/android/touringvancouver/FoodsActivity.java
+++ TouringVancouver/app/src/main/java/com/example/android/touringvancouver/FoodsActivity.java
@@ -6,14 +6,14 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class Category extends AppCompatActivity {
+public class FoodsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
-        // Create a list of words
+        // Create a list of attractions
         ArrayList<Attraction> attractions = new ArrayList<Attraction>();
         attractions.add(new Attraction("one", "lutti", R.drawable.van));
         attractions.add(new Attraction("two", "otiiko", R.drawable.van));
@@ -21,17 +21,17 @@ public class Category extends AppCompatActivity {
         attractions.add(new Attraction("four", "oyyisa", R.drawable.van));
         attractions.add(new Attraction("five", "massokka", R.drawable.van));
 
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
+        // Create an {@link AttractionAdapter}, whose data source is a list of {@link Attraction}s.
+        // The adapter knows how to create list items for each item in the list.
         AttractionAdapter adapter = new AttractionAdapter(this, attractions);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml file.
+        // category.xml file.
         ListView listView = (ListView) findViewById(R.id.list);
 
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
+        // Make the {@link ListView} use the {@link AttractionAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Attraction} in the list.
         listView.setAdapter(adapter);
     }
 }
--- TouringVancouver/app/src/main/java/com/example/android/touringvancouver/MainActivity.java
+++ TouringVancouver/app/src/main/java/com/example/android/touringvancouver/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         foods.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent foodsIntent = new Intent(MainActivity.this, Category.class);
+                Intent foodsIntent = new Intent(MainActivity.this, FoodsActivity.class);
                 startActivity(foodsIntent);
             }
         });
